07e66b77fdaf538cc3efd5983cab2072379c00f5
Yamy <yamy19007@gmail.com>
2023-07-14 13:16:18 +0200

    hm-1 * task-1 done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import './App.css'
-import logo from './logo.svg'
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <div className="simps">
+                <h3>Name: Homer Simpson</h3>
+                <h4>Age: 32</h4>
+                <h4>Gender: Male</h4>
+                <img
+                    src="https://i.discogs.com/J4bH_-A4UcQHFSUBDyyqXbTzr7XWM8S0NfNoYgwXAiI/rs:fit/g:sm/q:90/h:400/w:400/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTE0MDAz/MTctMTMzNTcxNzQ3/Ni5wbmc.jpeg"
+                    alt="Homer"
+                />
+            </div>
+            <div className="simps">
+                <h3>Name: Lisa Simpson</h3>
+                <h4>Age: 14</h4>
+                <h4>Gender: Female</h4>
+                <img
+                    src="https://logowik.com/content/uploads/images/lisa-simpson7517.logowik.com.webp"
+                    alt="Lisa"
+                />
+            </div>
+            <div className="simps">
+                <h3>Name: Bart Simpson</h3>
+                <h4>Age: 14</h4>
+                <h4>Gender: Male</h4>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png"
+                    alt="Bart"
+                />
+            </div>
+            <div className="simps">
+                <h3>Name: Maggie Simpson</h3>
+                <h4>Age: 2</h4>
+                <h4>Gender: Female</h4>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png"
+                    alt="Maggie"
+                />
+            </div>
+            <div className="simps">
+                <h3>Name: Marge Simpson</h3>
+                <h4>Age: 28</h4>
+                <h4>Gender: Female</h4>
+                <img
+                    src="https://www.drawing123.com/wp-content/uploads/2022/12/Drawing-Marge-Simpson-step-9-1.jpg"
+                    alt="Marge"
+                />
+            </div>
         </div>
     )
 }
 
 export default App
-// init homework-1
+
+//todo
+// 1. Описати всю сім'ю сімпсонів (5 персонажів). Характеристики : ім‘я приізвище вік стать, фотографія (посиланням на зовнішній ресурс,тобто не локальна фотка) Інфу брати звідки завгодно,хоч придумайте. Але для цього є сайт який ви юзали на жс
