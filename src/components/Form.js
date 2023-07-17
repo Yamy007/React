a4a2bb96f87e7c2603090a9b159777643f72658e
Yamy <yamy19007@gmail.com>
2023-07-17 14:33:35 +0200

    update task-2 add react hook form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,21 @@
-import React, { useRef } from 'react'
+import { DevTool } from '@hookform/devtools'
+import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const Form = () => {
-    const name = useRef(null)
-    const email = useRef(null)
-    const body = useRef(null)
+    const form = useForm()
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors },
+    } = form
 
-    const handelClick = e => {
-        e.preventDefault()
+    const onSubmit = data => {
         fetch('https://jsonplaceholder.typicode.com/posts/1', {
             method: 'PUT',
             body: JSON.stringify({
-                data: {
-                    name: name.current.value,
-                    email: email.current.value,
-                    body: body.current.value,
-                },
+                data: data,
             }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
@@ -23,20 +24,53 @@ const Form = () => {
             .then(response => response.json())
             .then(json => console.log(json))
     }
-
     return (
         <div>
-            <form onSubmit={handelClick}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <br />
-                <input type="text" placeholder="comment" ref={name} />
+                <input
+                    type="text"
+                    placeholder="Enter your Name"
+                    {...register('name', {
+                        required: 'Form is required',
+                        pattern: {
+                            value: /^.{4,25}$/,
+                            message: 'Invalid name',
+                        },
+                    })}
+                />
+                <p>{errors.name?.message}</p>
                 <br />
-                <input type="text" placeholder="info" ref={email} />
+                <input
+                    type="text"
+                    placeholder="Email address"
+                    {...register('email', {
+                        required: 'Email is required',
+                        pattern: {
+                            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                            message: 'Invalid Email',
+                        },
+                    })}
+                />
+                <p>{errors.email?.message}</p>
                 <br />
-                <input type="text" placeholder="info" ref={body} />
+                <input
+                    type="text"
+                    placeholder="Enter you comment"
+                    {...register('body', {
+                        required: 'Body is required',
+                        pattern: {
+                            value: /^.{3,255}/,
+                            message: 'Message must have more 3 characters and less 255',
+                        },
+                    })}
+                />
+                <p>{errors.body?.message}</p>
                 <br />
                 <button type="submit">Submit</button>
                 <br />
             </form>
+            <DevTool control={control} />
         </div>
     )
 }
